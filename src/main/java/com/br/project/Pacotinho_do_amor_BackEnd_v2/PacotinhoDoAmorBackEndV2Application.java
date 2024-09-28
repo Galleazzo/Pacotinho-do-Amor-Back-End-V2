145cf59e722633ed219fd85e1db699357d1fc640
Galleazzo <paulogalleazzo1@gmail.com>
2024-09-28 10:53:12 -0300

    first commit2

---
+++ src/main/java/com/br/project/Pacotinho_do_amor_BackEnd_v2/PacotinhoDoAmorBackEndV2Application.java
@@ -0,0 +1,13 @@
+package com.br.project.Pacotinho_do_amor_BackEnd_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacotinhoDoAmorBackEndV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacotinhoDoAmorBackEndV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/project/Pacotinho_do_amor_BackEnd_v2/PacotinhoDoAmorBackEndV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.project.Pacotinho_do_amor_BackEnd_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacotinhoDoAmorBackEndV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
